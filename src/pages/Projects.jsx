862cf3ab754f657eeb8cfa7ff0f983441446277f
Khurram Qureshi <khurram@fmpdev.net>
2023-10-03 14:36:03 +0500

    change project description

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -21,57 +21,39 @@ const Projects = () => {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ProjectCard
               imgPath={autobuffy}
               isBlog={false}
               title="Auto Buffy"
-              description="This online ride-sharing platform boasts a responsive front-end design, complete with an admin panel built using React and Material-UI. With the integration of Google Maps API, users can easily access directions on the map. The platform allows for full CURD functionality on profiles and posts, and users can confirm or cancel a ride from a post and message each other after confirmation. Additionally, users can easily reset their password. The back-end is powered by a Node Express server and utilizes MongoDB for the database management."
+              description="The AutoBuffy website functions as an E-commerce platform specializing in a diverse array of automotive spare parts and accessories. Here, customers can conveniently peruse the product catalog, choose the desired components, and proceed to make online orders. The platform collaborates with multiple vendors, offering a variety of payment options, including Stripe, PayPal, Afterpay, and Google Pay. AutoBuffy is constructed using Angular, Ruby On Rails (ROR), and utilizes a Postgres database."
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ProjectCard
               imgPath={buffyhub}
               isBlog={false}
               title="Buffy Hub"
-              description="This platform features a responsive interface built with Reactjs and CSS, allowing users to easily manage their profiles and posts through CRUD activities. The option to follow other users and interact with their posts (like or dislike) is also available, with suggestions for new users to follow. Users can only view posts from their followed users and can only engage in chat with those they follow. The backend is powered by Nodejs and Expressjs server, while MongoDB is utilized as the database."
+              description="The BuffyHub Inventory website is used to synchronize the inventory files from FTP and keep the main app https://autobuffy.com/ updated at all times. I have created this project using the following technologies. React, Ruby on Rails, Postgres. and deployed this app on AWS using Capistrano, and I have to use the following things on the server. EC2, RDS, S3 Bucket, Redis Cluster, Elastic cache, etc. this app is also deployed on AWS, a project management tool used Monday. com, for communication, use slack."
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={oneshade}
-              isBlog={false}
-              title="One Shade"
-              description="This ecommerce website utilizes a REST API built with Nodejs and Expressjs, allowing for easy CRUD operations on user profiles, products and admin panel. The user interface is designed for efficient product management and includes a complete purchase process. The admin panel, accessible for CRUD on users and products, includes a chart for statistics display. Stripe API is integrated to handle customer payments. The website uses MongoDB and Firebase as the database."
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ProjectCard
               imgPath={awaazdy}
               isBlog={false}
-              title="Awaaz Dy"
-              description="This blogging application features a RESTful API server built with Nodejs and Expressjs. The data is retrieved from the API server and displayed on a user-friendly interface designed using React, Redux, and styled-components for a seamless experience."
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={krypto}
-              isBlog={false}
               title="Research buddy"
-              description="Project Krypto is a Web 3 project that utilizes an Ethereum smart contract created using Solidity. It allows users to interact with their Metamask wallet and send Ethereum."
+              description="Research Buddy streamlines research for academics and researchers. It provides robust organization tools like tags, folders, and filters, simplifying document retrieval and online database searches. Notably, it automates citation and bibliography formatting in APA, MLA, and Chicago styles, saving time. Developed with React.js and Ruby on Rails, it employs Git, GitHub, Monday.com, and Slack for version control, project management, and team communication. Research Buddy is a versatile tool that enhances research workflows by offering organization, search capabilities, and automated citation support, powered by modern technologies and collaboration tools."
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ProjectCard
-              imgPath={kickstart}
+              imgPath={oneshade}
               isBlog={false}
               title="Nizam-e-Taleem"
-              description="Solidity Kickstart is a Web 3 project that utilizes an Ethereum smart contract created using Solidity. It includes all the functionality of a kickstart platform, enabling users to interact with their Metamask wallet, transfer Ethereum and participate in kickstart campaigns."
+              description="Nizame-Taleem is a school management system designed to help educational institutions manage their day-to-day operations. One of the key features of Nizame-Taleem is its student information management module, which allows schools to store and manage student data and academic records. The module includes tools for tracking attendance, exam marks, and generating reports. I have created this app using the following technologies. React js, Bootstrap, and RoR. and for version control using git and Bitbucket, project management tool use Monday. com, for communication, use slack."
             />
           </Col>
         </Row>
